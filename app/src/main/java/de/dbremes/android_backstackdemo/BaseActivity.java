7089e961e2372d3bb3cc9b861c05ce865b2b6353
dbremes <dbjobs@snafu.de>
2019-11-03 12:24:50 +0100

    Calling addToBackStack(); improved logging

--- app/src/main/java/de/dbremes/android_backstackdemo/BaseActivity.java
+++ app/src/main/java/de/dbremes/android_backstackdemo/BaseActivity.java
@@ -13,16 +13,16 @@ import java.util.List;
 @SuppressLint("Registered")
 public class BaseActivity extends AppCompatActivity {
     private static String TAG = "BaseActivity";
-    //TODO: targets of Back buttons OK?
-    //TODO: write about different events for add() vs. replace() of fragments
+
     protected void logBackStack() {
         final FragmentManager supportFragmentManager = getSupportFragmentManager();
+        final int backStackEntryCount = supportFragmentManager.getBackStackEntryCount();
         Log.d(TAG, "Back stack entries:");
-        if (supportFragmentManager.getBackStackEntryCount() == 0) {
+        if (backStackEntryCount == 0) {
             Log.d(TAG, "- (no back stack entries)");
         }
         else {
-            for (int i = 0; i < supportFragmentManager.getBackStackEntryCount(); i++) {
+            for (int i = 0; i < backStackEntryCount; i++) {
                 Log.d(TAG, "- " + supportFragmentManager.getBackStackEntryAt(i).getName());
             }
         }
@@ -32,24 +32,25 @@ public class BaseActivity extends AppCompatActivity {
     public void onBackPressed() {
         final int backStackEntryCount = getSupportFragmentManager().getBackStackEntryCount();
         final List<Fragment> fragments = getSupportFragmentManager().getFragments();
-        Log.d(TAG, "BaseActivity.onBackPressed(): backStackEntryCount = " + backStackEntryCount
+        Log.d(TAG, "onBackPressed(): backStackEntryCount = " + backStackEntryCount
                 // fragments.size() will always be 1 because there is 1 visible fragment at a time.
                 + "; fragments.size() = " + fragments.size());
-        Log.d(TAG, "BaseActivity.onBackPressed(): fragments.get(0) = "
+        Log.d(TAG, "onBackPressed(): fragments.get(0) = "
                 + fragments.get(0).getClass().getName());
         logBackStack();
     }
 
     void showFragment(Fragment fragment) {
-        Log.d(TAG, "BaseActivity.showFragment(): fragment=" + fragment.getClass().getName());
+        Log.d(TAG, "showFragment(): fragment=" + fragment.getClass().getName());
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, fragment, fragment.getClass().getName())
+                .addToBackStack(fragment.getClass().getName())
                 .commit();
     }
 
     void startActivity(Class activityClass) {
-        Log.d(TAG, "BaseActivity.startActivity(): activityClass=" + activityClass.getName());
+        Log.d(TAG, "startActivity(): activityClass=" + activityClass.getName());
         Intent intent = new Intent(this, activityClass);
         startActivity(intent);
     }
